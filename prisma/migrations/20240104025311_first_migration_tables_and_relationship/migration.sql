ce8c29d97fc8b4e8d7be7642a346e5b8fd8515b7
Guto <gutomourao05@gmail.com>
2024-01-03 23:55:08 -0300

    feat: changed relationship tables

--- prisma/migrations/20240104025311_first_migration_tables_and_relationship/migration.sql
+++ prisma/migrations/20240104025311_first_migration_tables_and_relationship/migration.sql
@@ -22,6 +22,7 @@ CREATE TABLE `adresses` (
     `number` INTEGER NOT NULL,
     `complement` VARCHAR(191) NULL,
     `isDefault` BOOLEAN NOT NULL DEFAULT false,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `userId` VARCHAR(191) NOT NULL,
@@ -39,6 +40,7 @@ CREATE TABLE `itens-menu` (
     `isActive` BOOLEAN NOT NULL DEFAULT true,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
+    `idItemOrder` VARCHAR(191) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -48,7 +50,7 @@ CREATE TABLE `itens-order` (
     `id` VARCHAR(191) NOT NULL,
     `quantity` INTEGER NOT NULL,
     `price` DOUBLE NOT NULL,
-    `idItenMenu` VARCHAR(191) NOT NULL,
+    `idOrder` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -58,7 +60,6 @@ CREATE TABLE `orders` (
     `id` VARCHAR(191) NOT NULL,
     `total` DOUBLE NOT NULL,
     `status` ENUM('WATTING_TO_BE_ACCEPET', 'PREPARED', 'OUT_FOR_DELIVERY', 'CONCLUDED', 'CANCELED') NOT NULL DEFAULT 'WATTING_TO_BE_ACCEPET',
-    `idItenOrder` VARCHAR(191) NOT NULL,
     `idUser` VARCHAR(191) NOT NULL,
     `idAddress` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -72,10 +73,10 @@ CREATE TABLE `orders` (
 ALTER TABLE `adresses` ADD CONSTRAINT `adresses_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `itens-order` ADD CONSTRAINT `itens-order_idItenMenu_fkey` FOREIGN KEY (`idItenMenu`) REFERENCES `itens-menu`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `itens-menu` ADD CONSTRAINT `itens-menu_idItemOrder_fkey` FOREIGN KEY (`idItemOrder`) REFERENCES `itens-order`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `orders` ADD CONSTRAINT `orders_idItenOrder_fkey` FOREIGN KEY (`idItenOrder`) REFERENCES `itens-order`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `itens-order` ADD CONSTRAINT `itens-order_idOrder_fkey` FOREIGN KEY (`idOrder`) REFERENCES `orders`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `orders` ADD CONSTRAINT `orders_idUser_fkey` FOREIGN KEY (`idUser`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
